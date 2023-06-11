3068140ceac1f31989575dabcf37ea00cc174463
Vadym Popoilyk <vadya.popoilyk@gmail.com>
2023-06-11 11:54:23 +0200

    new files

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict'
+
+
+for(let i = 2; i > 0; i--) {
+    let myName = "Vadym";
+    console.log(myName)
+}
+let myName = "Vasia"
+console.log(myName)
+
+const BLOCK_SIZE = 14 + 15;
+
+let user = "Vasia", age = 36;
+
+function getsize() {
+    var size = 15;
+}
+getsize()
+console.log(size)
+qwerty = 11;
+console.log(qwerty);
+var qwerty;
